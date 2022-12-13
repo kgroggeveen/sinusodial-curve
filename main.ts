0e1a504f75a34cf602a67ecdd71306f4b770b344
kgroggeveen <120437567+kgroggeveen@users.noreply.github.com>
2022-12-12 21:53:38 -0700

    test

--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
 radio.onReceivedValue(function (name, value) {
     if ("x" == name) {
-        serial.writeValue(name, value)
+        serial.writeValue(name, Math.abs(value - 180))
     }
 })
 led.toggle(0, 0)
 radio.setGroup(1)
 basic.forever(function () {
-    radio.sendValue("x", input.acceleration(Dimension.X))
+    radio.sendValue("x", input.compassHeading())
     basic.pause(10)
 })
